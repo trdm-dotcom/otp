3335ab080c0be573d75f3a39335fb54bad323ff2
Duc Minh <tranfminh@gmail.com>
2023-02-06 14:51:23 +0700

    init redis service

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import config from './Config';
 import RequestHandler from './consumer/RequestHandler';
 import { Logger, Kafka } from 'common';
 import { Container } from 'typedi';
+import RedisService from './service/RedisService';
 
 Logger.create(config.logger.config, true);
 Logger.info('Starting...');
@@ -23,7 +24,16 @@ function init() {
             config.kafkaConsumerOptions,
             {}
         );
-        Container.get(RequestHandler).init();
+        Promise.all([
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                Container.get(RequestHandler).init()
+                resolve(`init container RequestHandler`);
+            }),
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                Container.get(RedisService).init()
+                resolve(`init container RedisService`);
+            }),
+        ]);
     } catch (error) {
         Logger.error(error);
         process.exit(1);
