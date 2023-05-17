28174596c6f997f54637e7ab35e156cd467aa352
Duc Minh <tranfminh@gmail.com>
2023-05-17 14:36:18 +0700

    update RedisService throw GeneralError

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,23 +1,22 @@
 export default class {
-    public static INVALID_TYPE = 'INVALID_TYPE';
-    public static INVALID_ID = 'INVALID_ID';
-    public static INVALID_VALUE = 'INVALID_ID';
-    public static INVALID_ID_TYPE = 'INVALID_ID_TYPE';
-    public static OBJECT_NOT_FOUND = 'OBJECT_NOT_FOUND';
-    public static OTP_GENERATE_TO_FAST = 'OTP_GENERATE_TO_FAST';
-    public static OTP_LIMIT_GENERATE = 'OTP_LIMIT_GENERATE';
-    public static OTP_TEMPORARILY_LOCKED = 'OTP_TEMPORARILY_LOCKED';
-    public static INCORRECT_OTP = 'INCORRECT_OTP';
-    public static OTP_ID_INVALID = 'OTP_ID_INVALID';
-    public static INTERNAL_ERROR = 'INTERNAL_ERROR';
-    public static SUBJECT = {
-        RESET_PASSWORD: {
-            vi: '',
-            en: ''
-        },
-        VERIFY: {
-            vi: '',
-            en: ''
-        }
-    }
+  public static INVALID_TYPE = 'INVALID_TYPE';
+  public static INVALID_ID = 'INVALID_ID';
+  public static INVALID_VALUE = 'INVALID_ID';
+  public static INVALID_ID_TYPE = 'INVALID_ID_TYPE';
+  public static OBJECT_NOT_FOUND = 'OBJECT_NOT_FOUND';
+  public static OTP_GENERATE_TO_FAST = 'OTP_GENERATE_TO_FAST';
+  public static OTP_LIMIT_GENERATE = 'OTP_LIMIT_GENERATE';
+  public static OTP_TEMPORARILY_LOCKED = 'OTP_TEMPORARILY_LOCKED';
+  public static INCORRECT_OTP = 'INCORRECT_OTP';
+  public static OTP_ID_INVALID = 'OTP_ID_INVALID';
+  public static SUBJECT = {
+    RESET_PASSWORD: {
+      vi: '',
+      en: '',
+    },
+    VERIFY: {
+      vi: '',
+      en: '',
+    },
+  };
 }
